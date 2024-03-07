d393d18babd27cdea8a103059c2a1a2be3c9bf94
jami1024 <996013797@qq.com>
2024-03-07 22:22:31 +0800

    解决添加任务重复问题

--- internal/domain/tasks.go
+++ internal/domain/tasks.go
@@ -4,10 +4,11 @@ import clientv3 "go.etcd.io/etcd/client/v3"
 
 // Task 定时任务
 type Task struct {
-	Name     string `json:"name" binding:"required"`     //  任务名
-	Command  string `json:"command" binding:"required"`  // shell命令
-	CronExpr string `json:"cronExpr" binding:"required"` // cron表达式
-	Zk       string `json:"zk"`                          //用来表示哪个中控，或者理解成不同时区的集群，其中cron表达式要根据zk来写
+	Name       string `json:"name" binding:"required"`     //  任务名
+	Command    string `json:"command" binding:"required"`  // shell命令
+	CronExpr   string `json:"cronExpr" binding:"required"` // cron表达式
+	Zk         string `json:"zk"`                          //用来表示哪个中控，或者理解成不同时区的集群，其中cron表达式要根据zk来写
+	UniqueCode string `json:"uniqueCode"`                  // 用来拼接任务名称保证任务唯一，用户不用关注、程序自身赋值。
 }
 
 // TaskMgr 任务管理
